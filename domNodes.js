fce81ec1d692033d08bc538e420e0c2243df0e16
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2022-02-13 15:10:24 -0600

    CAmbio de la ultima clase

---
+++ domNodes.js
@@ -0,0 +1,9 @@
+
+let link = document.getElementsByTagName('a')[0]
+
+let title = document.getElementById('title')
+
+let subtitle = document.getElementsByClassName('subtitle')[0]
+console.log(link)
+console.log(title)
+console.log(subtitle)
